55b105cf3c7e279825598b519cdae5ea4903a83d
Martin Benjamins <marlamin@marlamin.com>
2018-12-27 23:33:12 +0100

    Remove tests dir

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -8,11 +8,14 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
+            //var build = "8.0.1.26287";
             var build = "8.1.0.28366";
             var dbcdir = @"Z:\DBCs\" + build + @"\dbfilesclient";
 
             foreach (var file in Directory.GetFiles(dbcdir))
             {
+                //if (Path.GetFileNameWithoutExtension(file) != "mountxdisplay")
+                    //continue;
                 try
                 {
                     var definition = DefinitionManager.CompileDefinition(file, build);
