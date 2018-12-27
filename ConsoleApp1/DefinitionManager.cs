0fdbb55f6186a5f8380ab3ceac7efd133b57e96b
Martin Benjamins <marlamin@marlamin.com>
2018-12-28 00:02:04 +0100

    Fix up test app

--- ConsoleApp1/DefinitionManager.cs
+++ ConsoleApp1/DefinitionManager.cs
@@ -4,8 +4,8 @@ using System.IO;
 using DBDefsLib;
 using System.Reflection;
 using System.Reflection.Emit;
-using CascStorageLib.Attributes;
-using CascStorageLib;
+using DB2FileReaderLib.NET;
+using DB2FileReaderLib.NET.Attributes;
 
 namespace ConsoleApp1
 {
--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,6 +1,6 @@
 ﻿using System;
 using System.IO;
-using CascStorageLib;
+using DB2FileReaderLib;
 
 namespace ConsoleApp1
 {
